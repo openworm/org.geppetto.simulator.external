7de08b19405768595dce5f14a03f24bff05888a4
jrmartin <yeshuarmartinez@gmail.com>
2015-01-29 21:25:08 -0800

    removing processes classes that were moved to core

---
+++ src/main/java/org/geppetto/simulator/external/services/NeuronSimulatorService.java
@@ -0,0 +1,100 @@
+package org.geppetto.simulator.external.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geppetto.core.beans.SimulatorConfig;
+import org.geppetto.core.common.GeppettoExecutionException;
+import org.geppetto.core.common.GeppettoInitializationException;
+import org.geppetto.core.externalprocesses.ExternalProcessWatcher;
+import org.geppetto.core.model.IModel;
+import org.geppetto.core.model.ModelInterpreterException;
+import org.geppetto.core.model.ModelWrapper;
+import org.geppetto.core.model.RecordingModel;
+import org.geppetto.core.model.runtime.AspectNode;
+import org.geppetto.core.simulation.IRunConfiguration;
+import org.geppetto.core.simulation.ISimulatorCallbackListener;
+import org.geppetto.core.simulator.AExternalProcessSimulator;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Neuron Simulator 
+ * 
+ * @author Jesus R Martinez (jesus@metacell.us)
+ *
+ */
+public class NeuronSimulatorService extends AExternalProcessSimulator{
+
+	private static Log _logger = LogFactory.getLog(NeuronSimulatorService.class);
+
+	@Autowired
+	private SimulatorConfig neuronSimulatorConfig;
+
+	private String NEURON_HOME = "/usr/bin/nrn";
+	
+	@Override
+	public void initialize(List<IModel> models, ISimulatorCallbackListener listener) throws GeppettoInitializationException, GeppettoExecutionException
+	{
+		this.createProcessCommand(null);
+	}
+	
+	@Override
+	public String getName() {
+		return this.neuronSimulatorConfig.getSimulatorName();
+	}
+
+	@Override
+	public String getId() {
+		return this.neuronSimulatorConfig.getSimulatorID();
+	}
+
+	public void createProcessCommand(String originalFileName){
+
+		try{
+			File filePath = new File(originalFileName);
+			String command = null;
+
+			String directoryToExecuteFrom = filePath.getCanonicalPath();
+
+			if(filePath.isDirectory()){
+				command = NEURON_HOME
+						+ System.getProperty("file.separator")
+						+ "bin"
+						+ System.getProperty("file.separator")
+						+ "nrnivmodl";
+			}else{
+				String extension = "";
+
+				int i = filePath.getAbsolutePath().lastIndexOf('.');
+				if (i > 0) {
+					extension = filePath.getAbsolutePath().substring(i+1);
+				}
+
+				_logger.info("File with extension " + extension + " detected");
+
+				directoryToExecuteFrom = filePath.getParentFile().getAbsolutePath();
+				if(extension.equals("hoc")){
+					command = NEURON_HOME
+							+ System.getProperty("file.separator")
+							+ "bin"
+							+ System.getProperty("file.separator")
+							+ "nrngui " + filePath.getAbsolutePath();
+				}
+				else if(extension.equals("py")){
+					command = "python " + filePath.getAbsolutePath();
+				}
+
+				_logger.info("Command to Execute: " + command + " ...");
+				_logger.info("From directory : " + directoryToExecuteFrom);
+			}
+
+			this.runExternalProcess(command, directoryToExecuteFrom, originalFileName);
+		}
+		catch(IOException e){
+
+		}
+	}
+}
