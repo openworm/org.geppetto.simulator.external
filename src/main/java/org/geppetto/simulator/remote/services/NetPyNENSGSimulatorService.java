14655e872625d58d6601b30c548489350fd0d311
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-06-13 13:40:13 +0100

    working on netpyne integration

--- src/main/java/org/geppetto/simulator/remote/services/NetPyNENSGSimulatorService.java
+++ src/main/java/org/geppetto/simulator/remote/services/NetPyNENSGSimulatorService.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.geppetto.core.beans.SimulatorConfig;
 import org.geppetto.core.services.registry.ServicesRegistry;
 import org.geppetto.model.ModelFormat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,12 +19,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class NetPyNENSGSimulatorService extends NSGSimulatorService
 {
-
+	@Autowired
+	private SimulatorConfig netPyNENSGSimulatorConfig;
+	
 	@Override
 	public void registerGeppettoService()
 	{
-		List<ModelFormat> modelFormats = new ArrayList<ModelFormat>(Arrays.asList(ServicesRegistry.registerModelFormat("NetPyNE")));
+		List<ModelFormat> modelFormats = new ArrayList<ModelFormat>(Arrays.asList(ServicesRegistry.registerModelFormat("NETPYNE")));
 		ServicesRegistry.registerSimulatorService(this, modelFormats);
+	}
+	
+	@Override
+	public String getName()
+	{
+		return this.netPyNENSGSimulatorConfig.getSimulatorName();
+	}
+
+	@Override
+	public String getId()
+	{
+		return this.netPyNENSGSimulatorConfig.getSimulatorID();
+	}
+
+
+	/**
+	 * @param neuronSimulatorConfig
+	 * @deprecated for test purposes only, the configuration is autowired
+	 */
+	public void setNeuronSimulatorConfig(SimulatorConfig netPyNENSGSimulatorConfig)
+	{
+		this.netPyNENSGSimulatorConfig = netPyNENSGSimulatorConfig;
 	}
 
 }
